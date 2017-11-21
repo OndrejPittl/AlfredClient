caac056a59b682321753d6356c179b8791508d5a
Ondrej Pittl <ondrejpittl@jaroslav-sevcik-nb.etnetera.mh.etn>
2017-11-21 12:12:46 +0100

    comments

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,20 +1,26 @@
 var app = angular.module("alfred", []);
 
 app.controller('feedController', function($rootScope, $element) {
-  salvattore.recreateColumns($element[0]);
 
+  // při route change: recall
+  salvattore.recreateColumns($element[0]);
+  
+
+  // nefunguje
   $rootScope.$on("$routeChangeStart", function (event, current, previous, rejection) {
     console.log($scope, $rootScope, $route, $location);
   });
 
+  // nefunguje
   $rootScope.$on("$routeChangeSuccess", function (event, current, previous, rejection) {
     console.log($scope, $rootScope, $route, $location);
   });
 });
 
 
-
 app.run(function($rootScope, $location) {
+
+  // nefunguje
   $rootScope.$on( "$routeChangeStart", function(event, next, current) {
     console.log("xxxx1");
   });
